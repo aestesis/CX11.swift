0421bc752515a84ddf5c16c8d7b82c20bd448f0d
renan <renan@aestesis.org>
2017-10-05 01:15:52 +0200

    name changed

--- Package.swift
+++ Package.swift
@@ -1,7 +1,15 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
-    name: "CX11.swift"
+    name: "X11",
+    products: [
+        .library(name: "X11",targets: ["X11"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "X11",dependencies: [])
+    ]
 )
